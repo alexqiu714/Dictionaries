d7a9ac5acf49296d11bf09858c7f14aa8cdbec7e
alexqiu714 <haode.qiu@isbasel.ch>
2025-11-18 17:20:46 +0100

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,18 @@
+alphabet = {"a" : 0, "b" : 0, "c" : 0, "d" : 0, "e" : 0, "f" : 0, "g" : 0, "h" : 0, "i" : 0, "j" : 0, "k" : 0, "l" : 0, "m" : 0, "n" : 0, "o" : 0, "p" : 0, "q" : 0, "r" : 0, "s" : 0, "t" : 0, "u" : 0, "v" : 0, "w" : 0, "x" : 0, "y" : 0, "z" : 0}
+x = input("What is your name?")
+
+for i in x:
+    if i in alphabet:
+        alphabet[i] += 1
+print(alphabet)
+
+pangram = True
+for count in alphabet.values():
+    if count == 0:
+        pangram = False
+
+if pangram == True:
+    print("Your name is a pangram")
+
+else:
+    print("Your name is not a pangram")
